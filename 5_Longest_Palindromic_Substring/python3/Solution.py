9b6c5532e6ac7af4c4879fe0fee3bb383cb4253f
FxmousKid <iyan005@outlook.com>
2024-06-15 20:10:40 +0300

    Subject: Adding Validated python3 solution to 5 : Longest Palindromic Substring
    Body:
    	- Problem Name : Longest Palindromic Substring
    	- Problem Number : 5
    	- Difficulty : Medium
    	- Status : Completed
    	- Language : Python
    
    Footer: First tried a Dumb approch with recursion, wayy too slow, then
    tried to implement Manacher's algorith, but with little explanation i
    founmd, i decided to simply implement a Expand from Center approach

---
+++ 5_Longest_Palindromic_Substring/python3/Solution.py
@@ -0,0 +1,61 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Solution.py                                        :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/06/15 17:38:19 by inazaria          #+#    #+#              #
+#    Updated: 2024/06/15 20:10:30 by inazaria         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import sys
+
+# Time to Implement Manacher's Algorithm !
+# Nevermind it's tough, if anyone expects it in a interview, i might as well
+# expect CEO position from them hahaha
+# we're implementing expand around center method, O(n^2) time, but O(1) space
+
+class Solution:
+    def expandAroundCenter(self, s, left, right) -> str :
+
+        while (left >= 0 and right < len(s) and s[right] == s[left]) :
+            left -= 1
+            right += 1
+
+        return (s[left + 1 : right])
+        
+
+    def longestPalindrome(self, s: str) -> str:
+        
+        if (len(s) <= 1) :
+            return s
+    
+        longest: str = ""
+        for i in range(len(s)) :
+            odd_palindrome_substr: str = self.expandAroundCenter(s, i, i)
+            even_palindrome_substr: str = self.expandAroundCenter(s, i, i + 1)
+       
+            print(f"odd_palindrome_substr = {odd_palindrome_substr}")
+            print(f"even_palindrome_substr = {even_palindrome_substr}")
+            print("\n")
+
+            if len(odd_palindrome_substr) > len(longest) :
+                longest = odd_palindrome_substr
+
+            if len(even_palindrome_substr) > len(longest) :
+                longest = even_palindrome_substr
+
+        return longest
+
+
+if __name__ == "__main__" :
+    if (len(sys.argv) != 2) :
+        print("Usage : python Solution.py <string>")
+        sys.exit(1)
+ 
+    string: str = sys.argv[1]
+    answer: str = Solution().longestPalindrome(string)
+    print(f"Longest Palindrome in {string} : {answer}")
+    sys.exit(0)
