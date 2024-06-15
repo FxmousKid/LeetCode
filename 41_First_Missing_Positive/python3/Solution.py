d7ac491517f53cca06ec47b9b1537be35a38be18
FxmousKid <iyan005@outlook.com>
2024-06-15 03:46:28 +0300

    Subject: Adding Work in Progress to python3 solution to 41 : First Missing Positive
    Body:
    	- Problem Name : First Missing Positive
    	- Problem Number : 41
    	- Difficulty : Hard
    	- Status : WiP
    	- Language : Python
    
    Footer:

--- 41_First_Missing_Positive/python3/Solution.py
+++ 41_First_Missing_Positive/python3/Solution.py
@@ -6,16 +6,24 @@
 #    By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/06/14 19:21:11 by inazaria          #+#    #+#              #
-#    Updated: 2024/06/14 19:27:30 by inazaria         ###   ########.fr        #
+#    Updated: 2024/06/15 01:28:22 by inazaria         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
-from typing import List
+from typing import List, Union
 
 class Solution:
-    def firstMissingPositive(self, nums: List[int]) -> int:
-       return 1 
+    def firstMissingPositive(self, nums: List[int]) -> int :
+        positive_list: List[Union[int, str]]
+
+        first_missing_positive: int = 1
+        for num in nums :
+            
+            if num > 0 and 
+
+
+        return first_missing_positive
 
 if __name__ == "__main__" :
     if (len(sys.argv) != 2) :
